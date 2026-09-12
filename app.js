0ced08980ffef172f213f36765283d9183e2a0ab
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-12 01:54:06 +0100

    Re-added GET /books route after accidental deletion

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(router);
 
 app.get('/', (req, res) => {
-    return res.status(200).json({ message: 'Server is running' });
+  return res.status(200).json({ message: 'Server is running' });
 });
 
 export default app;
