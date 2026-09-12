920532c5f5991d014bf0c1f33c8414075f4e170b
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-12 02:22:02 +0100

    Implemented GET /books/:id route

--- src/models/books.js
+++ src/models/books.js
@@ -7,4 +7,11 @@ const getAllBooks = async () => {
   return books;
 };
 
-export { getAllBooks };
+const getBookById = async (id) => {
+  const db = getDb();
+  const collection = db.collection('books');
+  const book = await collection.findOne({ id: id });
+  return book;
+};
+
+export { getAllBooks, getBookById };
