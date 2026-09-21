6aca60db39acec570d402430a6a39122ee959e42
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-21 22:33:46 +0100

    Swagger Documentation

--- src/router.js
+++ src/router.js
@@ -5,16 +5,253 @@ import { getAuthorsHandler, getAuthorByIdHandler, addAuthorHandler, deleteAuthor
 const router = express.Router();
 
 // BOOK routes
+
+/**
+ * @openapi
+ * /books:
+ *   get:
+ *     summary: Get all books
+ *     tags:
+ *       - Books
+ *     responses:
+ *       200:
+ *         description: Books returned successfully
+ *       500:
+ *         description: Unable to retrieve books
+ */
 router.get('/books', getBooksHandler);
+
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   get:
+ *     summary: Get one book by id
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The custom book id, such as b1
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Book returned successfully
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Unable to retrieve book
+ */
 router.get('/books/:id', getBookByIdHandler);
+
+/**
+ * @openapi
+ * /books:
+ *   post:
+ *     summary: Add a new book
+ *     tags:
+ *       - Books
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - id
+ *               - title
+ *               - authorId
+ *               - publicationDate
+ *             properties:
+ *               id:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               authorId:
+ *                 type: string
+ *               publicationDate:
+ *                 type: string
+ *           example:
+ *             id: b1
+ *             title: Things Fall Apart
+ *             authorId: a1
+ *             publicationDate: 1958
+ *     responses:
+ *       201:
+ *         description: Book added successfully
+ *       400:
+ *         description: Invalid book data
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/books', addBookHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   put:
+ *     summary: Update a book
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               authorId:
+ *                 type: string
+ *           example:
+ *             title: Updated Title
+ *             authorId: a1
+ *     responses:
+ *       200:
+ *         description: Book updated successfully
+ *       400:
+ *         description: Invalid book data
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Internal server error
+ */
 router.put('/books/:id', updateBookHandler);
+
+/**
+ * @openapi
+ * /books/{id}:
+ *   delete:
+ *     summary: Delete a book
+ *     tags:
+ *       - Books
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Book deleted successfully
+ *       404:
+ *         description: Book not found
+ *       500:
+ *         description: Internal server error
+ */
 router.delete('/books/:id', deleteBookHandler);
 
 // AUTHOR routes
+
+/**
+ * @openapi
+ * /authors:
+ *   get:
+ *     summary: Get all authors
+ *     tags:
+ *       - Authors
+ *     responses:
+ *       200:
+ *         description: Authors returned successfully
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/authors', getAuthorsHandler);
+
+/**
+ * @openapi
+ * /authors/{id}:
+ *   get:
+ *     summary: Get one author by id
+ *     tags:
+ *       - Authors
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Author returned successfully
+ *       404:
+ *         description: Author not found
+ *       500:
+ *         description: Internal server error
+ */
 router.get('/authors/:id', getAuthorByIdHandler);
+
+/**
+ * @openapi
+ * /authors:
+ *   post:
+ *     summary: Add a new author
+ *     tags:
+ *       - Authors
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - id
+ *               - name
+ *             properties:
+ *               id:
+ *                 type: string
+ *               name:
+ *                 type: string
+ *               bio:
+ *                 type: string
+ *           example:
+ *             id: a1
+ *             name: Chinua Achebe
+ *             bio: Nigerian novelist, poet, and critic
+ *     responses:
+ *       201:
+ *         description: Author added successfully
+ *       400:
+ *         description: Invalid author data
+ *       500:
+ *         description: Internal server error
+ */
 router.post('/authors', addAuthorHandler);
+
+/**
+ * @openapi
+ * /authors/{id}:
+ *   delete:
+ *     summary: Delete an author
+ *     tags:
+ *       - Authors
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Author deleted successfully
+ *       404:
+ *         description: Author not found
+ *       409:
+ *         description: Cannot delete author because books reference this author
+ *       500:
+ *         description: Internal server error
+ */
 router.delete('/authors/:id', deleteAuthorHandler);
+
+
 
 export default router;
