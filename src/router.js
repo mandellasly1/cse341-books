42d7c0cfa6ea3b11ea6998047077a1f2990713be
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-22 22:58:54 +0100

    cool

--- src/router.js
+++ src/router.js
@@ -277,50 +277,6 @@ router.put('/authors/:id', updateAuthorHandler);
 /**
  * @openapi
  * /authors/{id}:
- *   put:
- *     summary: Update an author
- *     tags:
- *       - Authors
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: The custom author id, such as a1
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - name
- *               - birthYear
- *             properties:
- *               name:
- *                 type: string
- *               birthYear:
- *                 type: integer
- *           example:
- *             name: Updated Author
- *             birthYear: 1981
- *     responses:
- *       200:
- *         description: Author updated successfully
- *       400:
- *         description: Invalid author data
- *       404:
- *         description: Author not found
- *       500:
- *         description: Internal server error
- */
-router.put('/authors/:id', updateAuthorHandler);
-
-
-/**
- * @openapi
- * /authors/{id}:
  *   delete:
  *     summary: Delete an author
  *     tags:
