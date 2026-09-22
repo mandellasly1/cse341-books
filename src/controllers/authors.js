3be70700f74931aaecdccbeeb49ba8238b0e59b5
mandellasly1 <akpomah_nelson@yahoo.com>
2026-09-22 21:27:04 +0100

    Add PUT /authors/:id route

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -53,6 +53,7 @@ const addAuthorHandler = async (req, res) => {
   }
 };
 
+
 // PUT update author
 const updateAuthorHandler = async (req, res) => {
   try {
@@ -63,6 +64,7 @@ const updateAuthorHandler = async (req, res) => {
       return res.status(400).json({ message: 'name and birthYear are required' });
     }
 
+    
     const result = await getDb().collection('authors').findOneAndUpdate(
       { id: authorId },
       { $set: updates },
@@ -72,6 +74,7 @@ const updateAuthorHandler = async (req, res) => {
     if (!result.value) {
       return res.status(404).json({ message: 'Author not found' });
     }
+
 
     return res.status(200).json(result.value);
   } catch (error) {
